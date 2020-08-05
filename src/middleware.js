ce4696a14c5104c0a8c45e81b8757b223c3ea1c2
Jefrey <jefrey.santos@capitual.io>
2020-08-05 13:31:42 -0300

    request body schema

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,31 @@
 // Request preprocessor
 // You can also populate the first argument of the routes calls
 // and get any defined property from 'req' (not only req.path)
-
 async function middleware(req, res, next) {
-  if (process.env.IP_HEADER && req.header(process.env.IP_HEADER))
-    req.ipAddr = req.header(process.env.IP_HEADER).split(",")[0]
-  else
-    req.ipAddr = req.connection.remoteAddress
+    let proceed = true
 
-  next()
+    if (process.env.IP_HEADER && req.header(process.env.IP_HEADER))
+        req.ipAddr = req.header(process.env.IP_HEADER).split(",")[0]
+    else
+        req.ipAddr = req.connection.remoteAddress
+
+    // for example, we can test request body schema	
+    if (req.route.schema) {
+        if (!req.body || typeof req.body != 'object')
+            req.body = {}
+
+        req.body = req.route.schema.stripUnknownProperties(req.body)
+        req.body = req.route.schema.cast(req.body)
+
+        try {
+            await req.route.schema.validate(req.body)
+        } catch (e) {
+            proceed = false
+            res.json(new BadDigestError(e))
+        }
+    }
+
+    proceed && next()
 }
 
 export default middleware
